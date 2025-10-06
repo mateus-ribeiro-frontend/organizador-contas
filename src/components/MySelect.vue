b630d7630a03f1b498fe166f0de97d031dc71f89
Mateus Rodrigues Ribeiro <mateus.ribeirovm@gmail.com>
2025-10-06 14:11:19 -0300

    feat novos componentes

---
+++ src/components/MySelect.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+    options: Array,
+    selected: Object
+})
+</script>
+
+<template>
+    <div>
+        <!--todo-->
+    </div>
+</template>
+
+<style scoped>
+.button {
+    border-radius: 16px;
+    min-width: 204px;
+    min-height: 40px;
+    align-self: center;
+    border-style: none;
+}
+
+.button-green {
+    background-color: #103308;
+}
+
+.button-red {
+    background-color: #410505;
+}
+
+.title {
+    color: white;
+    font-size: 24px;
+    font-weight: bolder;
+}
+</style>
---
+++ src/components/Overlay.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+
+import CloseBox from '../assets/close-box.svg'
+
+defineProps({
+  isOpen: Boolean
+})
+</script>
+
+<template>
+  <div v-if="true" class="overlay">
+    <div class="filter">
+      <header class="header">
+        <p>Filtro</p>
+        <CloseBox class="close" />
+      </header>
+      <slot name="body" />
+      <slot name="buttons" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.overlay {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.72);
+  height: 100vh;
+}
+
+.filter {
+  top: 40px;
+  position: relative;
+  background-color: #110517;
+}
+
+.header {
+  padding: 8px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.close {
+  width: 38px;
+  height: 38px;
+  fill: white;
+}
+</style>
